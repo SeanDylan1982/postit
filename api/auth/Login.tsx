f07b0db56296373dcbd59775d72f62e4bdefbbce
Sean Dylan Patterson <sean@seanpatterson.co.za>
2023-06-30 20:18:20 +0200

    OAuth

--- api/auth/Login.tsx
+++ api/auth/Login.tsx
@@ -5,7 +5,7 @@ import { signIn } from 'next-auth/react';
 export default function Login() {
   return (
     <li className="list-none">
-      <button className="text-sm" onClick={() => signIn()}>Sign In</button>
+      <button className="text-sm bg-gray-700 text-white py-2 px-6 rounded-xl disabled:opacity-25" onClick={() => signIn()}>Sign In</button>
     </li>
   )
 }
